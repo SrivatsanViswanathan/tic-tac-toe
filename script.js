872fbcda239fa9b5f12ef9f131627e6716e6646a
Srivatsan Viswanathan <sriv.viswanathan@gmail.com>
2022-06-17 19:33:52 -0400

    Multiplayer Somewhat works: - Player 1 and 2 can vs - Winner is displayed - 'New Game' button works but 'Play Again' has not been worked on

--- script.js
+++ script.js
@@ -46,19 +46,32 @@ const multiplayer = (() => {
     }
     const redo = () => {
         const homeScreen = document.querySelector('#restart');
-        const player1 = document.querySelector('#player1');
+        const player1Input = document.querySelector('#player1');
         const player1Title = document.querySelector('#player1-heading');
-        const player2 = document.querySelector('#player2');
+        const player2Input = document.querySelector('#player2');
         const player2Title = document.querySelector('#player2-heading');
+        const squares = document.querySelectorAll('.square');
+        const message = document.querySelector('#message');
         homeScreen.addEventListener('click', () => {
-            player1.value = '';
-            player2.value = '';
+            for (let i = 0; i < 9; i++) {
+                squares[i].textContent = '';
+                gameBoard.array[i] = '';
+                squares[i].setAttribute('value', 'false');
+            }
+            console.log(gameBoard.array);
+            player1Input.value = '';
+            player2Input.value = '';
             player1Title.textContent = 'Player 1';
             player2Title.textContent = 'Player 2';
-            console.log(player1.value);
             nameChange.classList.remove('active');
             buttonSubmit.classList.add('remove');
             nameChange.style.display = '';
+            player1 = players('Player 1', 'Player 1');
+            player2 = players('Player 2', 'Player 2');
+            xcount = 0;
+            ocount = 0;
+            message.textContent = 'Undecided';
+            message.style.visibility = 'hidden';
             newGame.restart();
         });
     }
@@ -79,43 +92,136 @@ const multiplayer = (() => {
 })();
 
 
-const players = (name1, name2) => {
+const players = (name, player) => {
     const player1 = document.querySelector('#player1-heading');
     const player2 = document.querySelector('#player2-heading');
-    console.log(name1);
-    const getName = () => {
-        if (name1 === '') {
-            console.log('s');
-            player1.textContent = 'Player 1';
-        }
-        else {
-            player1.textContent = name1;
-        }
-
-        if (name2 === '') {
-            player2.textContent = 'Player 2';
-        }
-        else {
-            player2.textContent = name2;
-        }
+    if (name === '') {
+        name = player;
     }
-    return {getName};
+    const getName = () => {
+        if (name === '') {
+            if (player === 'Player 1') {
+                player1.textContent = player;
+            }
+            else {
+                player2.textContent = player;
+            }
+        }
+        else {
+            if (player === 'Player 1') {
+                player1.textContent = name;
+            }
+            else if (player === 'Player 2') {
+                player2.textContent = name;
+            }
+        }
+        console.log('name', name);
+        winner();
+    }
+    return {getName, name, player};
 }
 const formInformation = (() => {
     const formButton = document.querySelector('#submit');
     const form = () => {
         formButton.addEventListener('click', () => {
-            const player1 = document.querySelector('#player1');
-            const player2 = document.querySelector('#player2');
-            const player = players(player1.value, player2.value);
-            player.getName();
-            player1.value = '';
-            player2.value = '';
+            const player1Name = document.querySelector('#player1');
+            const player2Name = document.querySelector('#player2');
+            player1 = players(player1Name.value, 'Player 1');
+            player2 = players(player2Name.value, 'Player 2');
+            player1.getName();
+            player2.getName();
+            player1Name.value = '';
+            player2Name.value = '';
+            console.log('name2', player1);
         });
     }
     return {form};
 
 })();
+
+const gameBoard = {
+    array: ['', '', '', '', '', '', '', '', ''],
+    array2: [['', '', ''], ['', '', ''], ['', '', '']]
+}
+
+const squares = document.querySelectorAll('.square');
+
+let xcount = 0;
+let ocount = 0;
+for (let i = 0; i < gameBoard.array.length; i++) {
+    squares[i].addEventListener('click', () => {
+        if (xcount === ocount && squares[i].getAttribute('value') != 'true') {
+            gameBoard.array[i] = 'X';
+        }
+        else if (xcount > ocount && squares[i].getAttribute('value') != 'true') {
+            gameBoard.array[i] = 'O';
+        }
+
+        squares[i].textContent = gameBoard.array[i];
+        if (gameBoard.array[i].includes('X') && squares[i].getAttribute('value') != 'true') {
+            xcount++;
+            squares[i].setAttribute('value', 'true');
+        }
+        if (gameBoard.array[i].includes('O') && squares[i].getAttribute('value') != 'true') {
+            ocount++;
+            squares[i].setAttribute('value', 'true');
+        }
+        winner();
+    });
+    
+}
+function winner() {
+    const message = document.querySelector('#message');
+    let winner = false;
+    let player = '';
+    for (let i = 0; i < 9; i = i + 3) {
+        if (gameBoard.array[i] === 'X' || gameBoard.array[i] === 'O') {
+            if (gameBoard.array[i] === gameBoard.array[i + 1] && gameBoard.array[i] === gameBoard.array[i + 2]) {
+                winner = true;
+                player = gameBoard.array[i];
+            }
+        }
+
+    }
+    for (let i = 0; i < 3; i++) {
+        if (gameBoard.array[i] === 'X' || gameBoard.array[i] === 'O') {
+            if (gameBoard.array[i] === gameBoard.array[i + 3] && gameBoard.array[i] === gameBoard.array[i + 6]) {
+                winner = true;
+                player = gameBoard.array[i];
+            }
+        }
+    }
+    for (let i = 0; i < 3; i = i + 2) {
+        if (gameBoard.array[i] === 'X' || gameBoard.array[i] === 'O') {
+            if (gameBoard.array[i] === gameBoard.array[i + 4] && gameBoard.array[i] === gameBoard.array[i + 8]) {   
+                winner = true;
+                player = gameBoard.array[i];
+            }
+            else if (i === 2 && gameBoard.array[i] === gameBoard.array[i + 2] && gameBoard.array[i] === gameBoard.array[i + 4]) {
+                winner = true;
+                player = gameBoard.array[i];
+            }
+        }
+    }
+    if (winner) {
+        message.style.visibility = 'visible';
+        if (player === 'X') {
+            message.textContent = player1.name + ' is the Winner!';
+        }
+        else {
+            console.log('s');
+            console.log(player2);
+            message.textContent = player2.name + ' is the Winner!';
+        }
+    }
+    else if (!winner && ((xcount + ocount) === 9)) {
+        message.style.visibility = 'visible';
+        message.textContent = 'Tie Game!';
+    }
+}
+
+let player1 = players('Player 1', 'Player 1');
+let player2 = players('Player 2', 'Player 2');
 
 newGame.display();
 multiplayer.modal();
